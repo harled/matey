a488608ef22eb0c1a16a6f3b20bce525eae1c2c9
Bradley Leonard <github@technickel.dev>
2022-03-15 22:28:50 +0000

    Add style, overwrite defaults

--- app/components/activeUsers.js
+++ app/components/activeUsers.js
@@ -1,32 +1,41 @@
 const template = document.createElement('template')
 template.innerHTML = `
-  <div class="card widget-flat shadow-sm">
-    <div class="card-body">
-      <div id="first col" class="col">
-        <div id="top half col" class="row">
-          <div class="col m-2">
-            <slot name="header">
-              <h1 class="card-title">
-                100
-              </h1>
-            </slot>
-          </div>
-          <div class="col">
-            <slot name="body">
-              <p class="card-text">
-                + 50 %
-              </p>
-            </slot>
-          </div>
-        </div>
-        <div class="col">
-          <slot name="footer">
-            <h5 class="card-text my-2">
-              Active Users since 1 day ago
-            </h5>
-          </slot>
-        </div>
-      </div>
+  <style>
+    .container {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-rows: repeat(2, 1fr);
+      justify-items: center;
+      align-items: center;
+
+      background-color: #e3242b;
+      color: white;
+      box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
+
+      word-wrap: break-word;
+      border: 0;
+      border-radius: 0.5rem;
+      padding: 1.25rem;
+      min-width: 10rem;
+      max-width: 16rem;
+    }
+
+    .text {
+      grid-column: span 2;
+    }
+  </style>
+
+  <div class="container">
+    <slot name="number">
+      <h1><span id="number">###</span></h1>
+    </slot>
+    <slot name="stat">
+      <p><span id="symbol">+</span> <span id="trend">##</span> / <span id="percent">##</span> %<br>since last week</p>
+    </slot>
+    <div class="text">
+      <slot name="text">
+        <h2>Active users since<br><span id="date">####-##-##</span></h2>
+      </slot>
     </div>
   </div>
 `
@@ -36,6 +45,15 @@ class ActiveUsers extends HTMLElement {
     super()
       .attachShadow({ mode: "open" })
       .append(template.content.cloneNode(true))
+  }
+
+  connectedCallback() {
+    let possibleAttributes = ["number", "symbol", "trend", "percent", "date"]
+    possibleAttributes.forEach((attribute) => {
+      if (this.getAttribute(attribute)) {
+        this.shadowRoot.querySelector(`#${attribute}`).innerHTML = this.getAttribute(attribute)
+      }
+    })
   }
 }
 
