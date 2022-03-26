4fe335d534d58c12961ecbbe9a5b74bfe1428334
Suvasan Krishnasamy <suvasan19@gmail.com>
2022-03-26 12:14:22 -0400

    active users over time WIP

--- sample/app/javascript/controllers/application.js
+++ sample/app/javascript/controllers/application.js
@@ -5,5 +5,7 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
 export { application }
+import ApexCharts from 'apexcharts'
+window.ApexCharts = ApexCharts
+window.ApexCharts = require("apexcharts") // expose to window
