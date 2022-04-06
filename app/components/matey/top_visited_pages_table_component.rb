b6dabfac078a561fef96aaf991b2980e61bee9fc
Suvasan Krishnasamy <suvasan19@gmail.com>
2022-04-06 05:59:06 +0000

    Fix Components and Add New Activity

--- app/components/matey/top_visited_pages_table_component.rb
+++ app/components/matey/top_visited_pages_table_component.rb
@@ -1,13 +1,12 @@
-class TopVisitedPagesTableComponent < ViewComponent::Base
-    def initialize(events:, time_window: 1.week)
-        # Scope events to time period given
-        @events = events.where(time: time_period)
-    
-        # Group events by controller (:name) and action. Aggregate number of unique user actions
-        @user_count_by_event = @events.group(:name, "properties->>'action'").select(:user_id).distinct.count
-    
+class Matey::TopVisitedPagesTableComponent < ApplicationComponent
+    def initialize(events:, time_window: 1.week, count: 10)    
+         # Group events by controller (:name) and action. Aggregate number of unique user actions
+        @user_count_by_event = events.where(started_at: time_window.ago..).pluck(:landing_page).tally
+            
         # Filter for the top 10 elements by user actions
         @user_count_by_event = @user_count_by_event.sort_by { |controller_name_and_action, count| count }.last(10).reverse
+        
+        @time_window = time_window
     end
   end
   
