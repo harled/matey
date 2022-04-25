1484cb82521a7f64a9765e5b561e244569075da6
Chris Young <chris@harled.ca>
2022-04-25 16:23:39 -0400

    remove duplicate sample directory

--- sample/app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    ahoy.track "My first event", language: "Ruby"
-  end
-end
--- sample/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get "home/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index"
-end
