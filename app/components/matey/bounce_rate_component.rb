5eae24001feb206be0d506298b15d8609f98148c
Piranavan <piranavan.jeyakumar@ecn.forces.gc.ca>
2022-08-06 17:51:28 -0400

    pseudo-code in commented form

--- app/components/matey/bounce_rate_component.rb
+++ app/components/matey/bounce_rate_component.rb
@@ -1,5 +1,15 @@
 class Matey::BounceRateComponent < ApplicationComponent
-  def initialize(events: )
+  def initialize(visits: )
     
+    # First determine the total number of user sessions to the website
+    @total_number_of_user_visits = visits.count
+
+    # Then determine the number of pages visited in a session 
+    # Maybe check for all sessions that only have 1 event? I.e the user went to one page and hasn't visited any other pages (thereby not triggering any other events)
+
+    # Then scope to sessions where only ONE-PAGE was visited
+
+    # Get the percentage as #-One-Page-Sessions / Total-#-Of-Sessions
+
   end 
 end
