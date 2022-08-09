f3dd21418ca5c45d32c481daa4e9969e46bb56f2
Piranavan <piranavan.jeyakumar@ecn.forces.gc.ca>
2022-08-09 19:49:27 +0000

    some more pseudocode work

--- app/components/matey/bounce_rate_component.rb
+++ app/components/matey/bounce_rate_component.rb
@@ -6,6 +6,7 @@ class Matey::BounceRateComponent < ApplicationComponent
 
     # Then determine the number of pages visited in a session 
     # Maybe check for all sessions that only have 1 event? I.e the user went to one page and hasn't visited any other pages (thereby not triggering any other events)
+    
 
     # Then scope to sessions where only ONE-PAGE was visited
 
