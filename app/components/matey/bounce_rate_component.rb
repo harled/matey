f51546d511d71073d42893af222a8faea5bf5fc6
Piranavan <piranavan.jeyakumar@ecn.forces.gc.ca>
2022-08-11 16:15:11 +0000

    playing around with users & visits, and determining how the valeus are calculated

--- app/components/matey/bounce_rate_component.rb
+++ app/components/matey/bounce_rate_component.rb
@@ -1,16 +1,21 @@
+require "ahoy_matey"
+
 class Matey::BounceRateComponent < ApplicationComponent
-  def initialize(visits: )
+  def initialize(visits:, users:)
     
     # First determine the total number of user sessions to the website
+      # visits/events have a NIL user_id associated with them
     @total_number_of_user_visits = visits.count
+      # users array is EMPTY (users aren't being created?)
+    @number_users = users.count
 
     # Then determine the number of pages visited in a session 
-    # Maybe check for all sessions that only have 1 event? I.e the user went to one page and hasn't visited any other pages (thereby not triggering any other events)
+    # Maybe check for all visits that only have 1 event? I.e the user went to one page and hasn't visited any other pages (thereby not triggering any other events)
     
 
     # Then scope to sessions where only ONE-PAGE was visited
 
-    # Get the percentage as #-One-Page-Sessions / Total-#-Of-Sessions
+    # Get the percentage as #-One-Page-Visits / Total-#-Of-Visits
 
   end 
 end
