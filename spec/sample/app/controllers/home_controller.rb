16c67cebbff910adee5c68e40fb7ff2b419feb03
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-08-12 15:54:52 -0400

    adjust data

--- spec/sample/app/controllers/home_controller.rb
+++ spec/sample/app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   def index
     ahoy.track "My first event", language: "Ruby"
     if rand(10) > 5
-      ahoy.track "My random event", language: "Ruby"
+      ahoy.track "My random event", language: "Ruby", action: "show"
     end
     if rand(25) > 12.5
-      ahoy.track "Another event", language: "Ruby"
+      ahoy.track "Another event", controller: "home"
     end
   end
 end
