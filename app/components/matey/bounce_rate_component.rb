d11f7c787534357d521c07d8b0b88a87d7d076b7
Piranavan <piranavan.jeyakumar@ecn.forces.gc.ca>
2022-08-13 03:56:57 +0000

    SQL magic to determine the total number of visits and single event visits using only the events table

--- app/components/matey/bounce_rate_component.rb
+++ app/components/matey/bounce_rate_component.rb
@@ -1,19 +1,16 @@
 require "ahoy_matey"
 
 class Matey::BounceRateComponent < ApplicationComponent
-  def initialize(visits:, users:)
-    
+  def initialize(events:)
+    raise ArgumentError unless events.is_a?(ActiveRecord::Relation)
+
     # First determine the total number of user sessions to the website
-      # visits/events have a NIL user_id associated with them
-    @total_number_of_user_visits = visits.count
-      # users array is EMPTY (users aren't being created?)
-    @number_users = users.count
+    @total_number_of_user_visits = events.pluck(:visit_id).uniq.count
 
-    # Then determine the number of pages visited in a session 
-    # Maybe check for all visits that only have 1 event? I.e the user went to one page and hasn't visited any other pages (thereby not triggering any other events)
+    # First we group by visit_id and scope to visits containing ONLY 1 event
+    # We then count each unique occurence of an visit with 1 event
+    @total_number_of_single_event_visits = events.group(:visit_id).having("count(ahoy_events.id) == 1").uniq.count
     
-
-    # Then scope to sessions where only ONE-PAGE was visited
 
     # Get the percentage as #-One-Page-Visits / Total-#-Of-Visits
 
