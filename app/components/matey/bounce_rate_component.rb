28ee2620cbdde4caddcae6c42ee3756d63d80714
Piranavan <piranavan.jeyakumar@ecn.forces.gc.ca>
2022-08-14 17:08:24 +0000

    organizing variables, less code, more variable names | added variable for percentage of bounce page visits  compared to total number of visits | added another variable to display bounce page path and its bounce count

--- app/components/matey/bounce_rate_component.rb
+++ app/components/matey/bounce_rate_component.rb
@@ -1,18 +1,23 @@
 require "ahoy_matey"
 
 class Matey::BounceRateComponent < ApplicationComponent
-  def initialize(events:)
-    raise ArgumentError unless events.is_a?(ActiveRecord::Relation)
+  def initialize(events:, visits:, limit: 5)
 
-    # First determine the total number of user sessions to the website
+    # Determine the total number of user sessions to the website
     @total_number_of_user_visits = events.pluck(:visit_id).uniq.count
 
     # First we group by visit_id and scope to visits containing ONLY 1 event
-    # We then count each unique occurence of an visit with 1 event
-    @total_number_of_single_event_visits = events.group(:visit_id).having("count(ahoy_events.id) == 1").uniq.count
-    
+    @visits_having_only_one_event = events.group(:visit_id).having("count(ahoy_events.id) == 1")
+    # We then count each unique occurence of a visit with 1 event
+    @total_number_of_single_event_visits = @visits_having_only_one_event.uniq.count
+
+    # Determine pages in which the most bounces occur
+    @single_event_visits_landing_page_count = visits.where(id: @visits_having_only_one_event.pluck(:visit_id)).pluck(:landing_page).tally
+
+    # Scope the results to the given limit and sort them in descending order
+    @most_bounced_pages = @single_event_visits_landing_page_count.sort_by { |controller_name_and_action, count| count }.last(limit).reverse
 
     # Get the percentage as #-One-Page-Visits / Total-#-Of-Visits
-
+    @percentage_of_visits_that_were_bounced = (@total_number_of_single_event_visits.to_f / @total_number_of_user_visits == 0 ? 1 : @total_number_of_user_visits )
   end 
 end
