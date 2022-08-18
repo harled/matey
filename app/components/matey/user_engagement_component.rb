b9ecc024a34c4019496c6b1f33e28cd4269b2b51
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-08-18 18:05:51 +0000

    create component to show most recent activity for user

--- app/components/matey/user_engagement_component.rb
+++ app/components/matey/user_engagement_component.rb
@@ -1,7 +1,10 @@
 require "ahoy_matey"
 
-class Matey::ActiveUsersComponent < ApplicationComponent
-  def initialize(event_name:, time_window: 1.week)
-
+class Matey::UserEngagementComponent < ApplicationComponent
+  def initialize(events:, user_id:, time_window: 1.week, limit: 10)
+    @events_for_user = events.where_props(user_id: user_id).group(:name).count
+    @count_by_event = @events_for_user.sort_by { |event, count| count }.first(limit)
+    @time_window = time_window
+    @user_id = user_id
   end
 end
--- spec/sample/app/controllers/home_controller.rb
+++ spec/sample/app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   def index
-    ahoy.track "My first event", language: "Ruby"
+    ahoy.track "My first event", language: "Ruby", user_id: 1 
     if rand(10) > 5
       ahoy.track "My random event", language: "Ruby"
+    end
+    if rand(100) > 50
+      ahoy.track "Other Event", user_id: 1
     end
   end
 end
