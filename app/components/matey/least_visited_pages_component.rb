eedd7d9b920577cc6b92bb1ce67c688ecc87ff06
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-08-22 16:42:54 +0000

    allow least visited pages component to take in properties

--- app/components/matey/least_visited_pages_component.rb
+++ app/components/matey/least_visited_pages_component.rb
@@ -1,17 +1,12 @@
 class Matey::LeastVisitedPagesComponent < ApplicationComponent
-  def initialize(events:, time_window: 1.year, limit: 10)
-    @events = events.where(time: time_window.ago..Time.current).group("properties -> 'controller'", "properties -> 'action'").count.sort_by { |k, v| v}.first(limit)
-    # for example:
-    # @events = events.where(time: time_window.ago..Time.current).group("properties -> 'controller'", "properties -> 'action'").count.sort_by { |k, v| v}.first(limit)
+  def initialize(events:, filter_property: nil, filter_value: nil, time_window: 1.year, limit: 10)
+    if filter_property && filter_value
+      @events = events.where_props("#{filter_property}": filter_value).where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v}.first(limit)
+    else
+      @events = events.where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v}.first(limit)
+    end
 
     @time_window = time_window
-
-    # grouped = events.where(time: 1.year.ago..Time.current).pluck(:properties)
-    # new_arr = grouped.group_by { |i| i}
-    # events = {}
-    # new_arr.keys.each do |i|
-    #   events[i] = new_arr[i].count
-    # end
-    # @events = events.sort_by { |k, v| v}
+    @filter_property = filter_property
   end
 end
