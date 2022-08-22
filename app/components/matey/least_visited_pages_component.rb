31adbe8e80d997daf0c455a5e79286fe5083a202
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-08-22 17:25:48 +0000

    spacing and linting

--- app/components/matey/least_visited_pages_component.rb
+++ app/components/matey/least_visited_pages_component.rb
@@ -1,9 +1,9 @@
 class Matey::LeastVisitedPagesComponent < ApplicationComponent
   def initialize(events:, filter_property: nil, filter_value: nil, time_window: 1.year, limit: 10)
-    if filter_property && filter_value
-      @events = events.where_props("#{filter_property}": filter_value).where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v}.first(limit)
+    @events = if filter_property && filter_value
+      events.where_props("#{filter_property}": filter_value).where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v }.first(limit)
     else
-      @events = events.where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v}.first(limit)
+      events.where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v }.first(limit)
     end
 
     @time_window = time_window
