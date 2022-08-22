2f7479c8efedd6f899b36083c710037952ec70e7
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-08-22 14:25:29 +0000

    resolve PR comments

--- app/components/matey/user_engagement_component.rb
+++ app/components/matey/user_engagement_component.rb
@@ -2,7 +2,7 @@ require "ahoy_matey"
 
 class Matey::UserEngagementComponent < ApplicationComponent
   def initialize(events:, user_id:, time_window: 1.week, limit: 10)
-    @events_for_user = events.where(time: time_window.ago..Time.current).where_props(user_id: user_id).group(:name).count
+    @events_for_user = events.where_props(user_id: user_id).where(time: time_window.ago..Time.current).group(:name).count
     @count_by_event = @events_for_user.sort_by { |event, count| count }.last(limit).reverse
     @time_window = time_window
     @user_id = user_id
