20148aa03f8cbcff7d262367334bf0c158aa4c5f
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-08-25 15:56:31 +0000

    lint with standard rb

--- app/components/matey/least_visited_pages_component.rb
+++ app/components/matey/least_visited_pages_component.rb
@@ -1,28 +1,28 @@
 class Matey::LeastVisitedPagesComponent < ApplicationComponent
-    
+
   def initialize(events:, filter_property: nil, filter_value: nil, time_window: 1.year, limit: 10)
-    def verify_props(prop, val, events)
-      hash = Hash.new
-      props = events.group(:properties).count
-
-      props.keys.each do|key|
-        key.each do |each_key|
-          hash[each_key] = 1
-        end
-      end
-      return hash.keys.include? [prop, val]
-    end
-
-    @verified_props = verify_props(filter_property, filter_value, events) 
-     @filter_value = filter_value if filter_value
+    @verified_props = verify_props(filter_property, filter_value, events)
+    @filter_value = filter_value if filter_value
     @filter_property = filter_property if filter_property
     @time_window = time_window
 
-    # verify that both prop and val have been passed in and that they are valid 
-    @events = if filter_property && filter_value && verify_props(filter_property, filter_value, events) 
+    # verify that both prop and val have been passed in and that they are valid
+    @events = if filter_property && filter_value && verify_props(filter_property, filter_value, events)
       events.where_props("#{filter_property}": filter_value).where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v }.first(limit)
     else
       events.where(time: time_window.ago..Time.current).group(:properties).count.sort_by { |k, v| v }.first(limit)
     end
   end
+
+  def verify_props(prop, val, events)
+      hash = {}
+      props = events.group(:properties).count
+
+      props.keys.each do |key|
+        key.each do |each_key|
+          hash[each_key] = 1
+        end
+      end
+      hash.key? [prop, val]
+    end
 end
