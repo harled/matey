e20837ca195e5af65f33d227133475c22fb5b3a7
Alex McDermid <alexander.mcdermid@gmail.com>
2022-09-18 01:42:48 +0000

    change from hash to db query

--- app/components/matey/browser_os_breakdown_component.rb
+++ app/components/matey/browser_os_breakdown_component.rb
@@ -2,19 +2,12 @@ class Matey::BrowserOsBreakdownComponent < ApplicationComponent
     def initialize(visits:, time_window:)
         @visits = visits
         
-        #creating a hash to find all the different browsers
-        #incrementing the hashes 
-        browsers = Hash.new
-        visits.each{ |visit| browsers.key?(visit.browser) ? browsers[visit.browser] += 1 : browsers[visit.browser] = 1 }
-        @browsers = browsers
-
-        oss = Hash.new
-        visits.each{ |visit| oss.key?(visit.os) ? oss[visit.os] += 1 : oss[visit.os] = 1 }
-        @oss = oss
+        @browsers = visits.group(:browser).count
+        @oss = visits.group(:os).count
 
         @view_count = visits.length()
 
-        #could probably just query the database
+        #would be very cool if we could somehow group by mac/windows/linux and then split into browsers (or the inverse)
 
         @safari = visits.where(browser: "Safari").take #take only returns one
         @chromes = visits.where(browser: "Chrome").all
