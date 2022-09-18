0bc79d48f1091a1cc4dc2ea8350f099a76afbe16
Alex McDermid <alexander.mcdermid@gmail.com>
2022-09-18 20:57:50 +0000

    distinct user visits by day

--- app/components/matey/daily_active_users_component.rb
+++ app/components/matey/daily_active_users_component.rb
@@ -2,5 +2,6 @@ class Matey::DailyActiveUsersComponent < ApplicationComponent
   def initialize(visits:, time_window:)
     @visits = visits
     @time_window = time_window
+    @distinct_user_visits_by_day = visits.order(:day).group(:day).uniq.count
   end
 end
