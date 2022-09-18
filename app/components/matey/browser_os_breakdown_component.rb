ef7be84472d77dda987e8e914dda89d8057db0a1
Alex McDermid <alexander.mcdermid@gmail.com>
2022-09-18 17:58:10 +0000

    linting

--- app/components/matey/browser_os_breakdown_component.rb
+++ app/components/matey/browser_os_breakdown_component.rb
@@ -1,17 +1,10 @@
 class Matey::BrowserOsBreakdownComponent < ApplicationComponent
-    def initialize(visits:, time_window:)
-        @visits = visits
-        # visits dont have time but started_at
-        visitsInTimeWindow = visits.where(started_at: time_window.ago..)
-        @visitsInTimeWindow = visitsInTimeWindow.count
-        
-        @browsers = visitsInTimeWindow.group(:browser).count
-        @oss = visitsInTimeWindow.group(:os).count
-
-        #TODO would be very cool if we could somehow group by mac/windows/linux and then split into browsers (or the inverse)
-
-        # @safari = visits.where(browser: "Safari").take #take only returns one
-        # @chromes = visits.where(browser: "Chrome").all
-        @time_window = time_window
-    end
+  def initialize(visits:, time_window:)
+    @visits = visits
+      visits_in_time_window = visits.where(started_at: time_window.ago..)
+      @visits_in_time_window = visits_in_time_window.count
+      @browsers = visits_in_time_window.group(:browser).count
+      @oss = visits_in_time_window.group(:os).count
+      @time_window = time_window
+  end
 end
