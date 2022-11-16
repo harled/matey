b67042c94dd5e8f75657e3b8cfcdfd14198d327e
Jesse Lu <jesselu@Jesses-MacBook-Pro.local>
2022-11-16 08:07:58 -0800

    further lint error checks

--- app/components/matey/visits_by_day_of_week_component.rb
+++ app/components/matey/visits_by_day_of_week_component.rb
@@ -8,9 +8,9 @@ class Matey::VisitsByDayOfWeekComponent < ApplicationComponent
     all_visits = visits.where(started_at: time_window.ago..)
 
     visits_by_day_of_week = {}
-    
+
     # get day of week from each visit. Incrase value of dayOfWeek key by 1 if key is already there else initialize key with value of 1
-    all_visits.each { |visit| visits_by_day_of_week.key?(visit.started_at.strftime("%A")) ? visits_by_day_of_week[(visit.started_at.strftime("%A"))] += 1 : visits_by_day_of_week[visit.started_at.strftime("%A")] = 1 }    
+    all_visits.each { |visit| visits_by_day_of_week.key?(visit.started_at.strftime("%A")) ? visits_by_day_of_week[(visit.started_at.strftime("%A"))] += 1 : visits_by_day_of_week[visit.started_at.strftime("%A")] = 1 }  
 
     # take out items from visits_by_day_of_week  hashmap based on exclude_days parameter
     if exclude_days.length > 0
@@ -18,6 +18,5 @@ class Matey::VisitsByDayOfWeekComponent < ApplicationComponent
     end
 
     @visits_by_day_of_week = visits_by_day_of_week
-
   end
 end
