8ddcab17bc0287ee992bc15450a6d21e4be7d3bc
Jesse Lu <jesselu@Jesses-MacBook-Pro.local>
2022-11-16 08:05:59 -0800

    fixed lint error checks

--- app/components/matey/visits_by_day_of_week_component.rb
+++ app/components/matey/visits_by_day_of_week_component.rb
@@ -2,23 +2,22 @@ require "ahoy_matey"
 
 class Matey::VisitsByDayOfWeekComponent < ApplicationComponent
   def initialize(visits:, time_window: 1.month, limit: 10, exclude_days: [])
-    
     @time_window = time_window
 
     # query for all the visits
     all_visits = visits.where(started_at: time_window.ago..)
 
-    visits_by_day_of_week = Hash.new 
-    # get day of week from each visit. Incrase value of dayOfWeek key by 1 if key is already there else initialize key with value of 1 
-    all_visits.each{ |visit| visits_by_day_of_week.key?(visit.started_at.strftime("%A")) ? visits_by_day_of_week[(visit.started_at.strftime("%A"))] += 1: visits_by_day_of_week[(visit.started_at.strftime("%A"))] = 1 }
+    visits_by_day_of_week = {}
     
+    # get day of week from each visit. Incrase value of dayOfWeek key by 1 if key is already there else initialize key with value of 1
+    all_visits.each { |visit| visits_by_day_of_week.key?(visit.started_at.strftime("%A")) ? visits_by_day_of_week[(visit.started_at.strftime("%A"))] += 1 : visits_by_day_of_week[visit.started_at.strftime("%A")] = 1 }    
+
     # take out items from visits_by_day_of_week  hashmap based on exclude_days parameter
-    if exclude_days.length > 0   
-      exclude_days.each{ |exclude| visits_by_day_of_week = visits_by_day_of_week.slice!(exclude) }
-    end 
+    if exclude_days.length > 0
+      exclude_days.each { |exclude| visits_by_day_of_week = visits_by_day_of_week.slice!(exclude) }
+    end
 
     @visits_by_day_of_week = visits_by_day_of_week
-    
 
   end
-end 
+end
