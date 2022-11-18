08f5f0000cb5de34f5438c1500aaa44d4d29f079
alexmcdermid <alexander.mcdermid@gmail.com>
2022-11-18 10:17:35 -0800

    lint

--- app/components/matey/daily_active_users_component.rb
+++ app/components/matey/daily_active_users_component.rb
@@ -4,6 +4,6 @@ class Matey::DailyActiveUsersComponent < ApplicationComponent
     @time_window = time_window
     visits_in_time_window = visits.where(started_at: time_window.ago..)
     @distinct_user_visits_by_day = visits_in_time_window.order(:day).group(:day).uniq.count
-    @distinct_user_visits_by_day2 = visits_in_time_window.order(:day).group('DATE(started_at)').map {|k,v| [k, v]}.sort
+    @distinct_user_visits_by_day2 = visits_in_time_window.order(:day).group("DATE(started_at)").map { |k, v| [k, v] }.sort
   end
 end
