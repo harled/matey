88bbb4f869fbc999b9000424fb8c75031eafccdc
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-09-14 18:14:00 +0000

    use database

--- app/components/matey/visits_by_day_of_week_component.rb
+++ app/components/matey/visits_by_day_of_week_component.rb
@@ -4,21 +4,19 @@ class Matey::VisitsByDayOfWeekComponent < Matey::ApplicationComponent
   def initialize(visits:, time_window: 1.month, limit: 10, exclude_days: [], color_scheme: "neutral")
     @time_window = time_window
 
-    # query for all the visits
-    all_visits = visits.where(started_at: time_window.ago..)
+    visits_by_day_of_week = visits
+      .where(started_at: time_window.ago..)
+      .group("to_char(started_at, 'Day')")
+      .order("count_all DESC")
+      .limit(limit)
+      .count
 
-    visits_by_day_of_week = {}
-
-    # get day of week from each visit. Incrase value of dayOfWeek key by 1 if key is already there else initialize key with value of 1
-    all_visits.each { |visit| visits_by_day_of_week.key?(visit.started_at.strftime("%A")) ? visits_by_day_of_week[(visit.started_at.strftime("%A"))] += 1 : visits_by_day_of_week[visit.started_at.strftime("%A")] = 1 }
-
-    # take out items from visits_by_day_of_week  hashmap based on exclude_days parameter
-    if exclude_days.length > 0
-      exclude_days.each { |exclude| visits_by_day_of_week = visits_by_day_of_week.slice!(exclude) }
+    # Remove days to be excluded
+    exclude_days.each do |day|
+      visits_by_day_of_week.delete(day.capitalize)
     end
 
     @visits_by_day_of_week = visits_by_day_of_week
-
     @color_scheme = color_scheme(scheme: color_scheme)
   end
 end
