4cf3e075dbf5314ce223bb1970472fed590b6f75
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-09-14 18:56:04 +0000

    rake standard fix

--- app/components/matey/visits_by_day_of_week_component.rb
+++ app/components/matey/visits_by_day_of_week_component.rb
@@ -10,7 +10,7 @@ class Matey::VisitsByDayOfWeekComponent < Matey::ApplicationComponent
       .count
 
     # Map numerical days back to day names
-    days = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+    days = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
     visits_by_day_of_week = visits_by_day_of_week.transform_keys { |k| days[k.to_i] }
 
     # Remove days to be excluded
