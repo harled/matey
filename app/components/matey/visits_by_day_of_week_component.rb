327701a119b7db7dfdd3b87018662061c9a4f199
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-09-14 19:05:30 +0000

    use groupdate

--- app/components/matey/visits_by_day_of_week_component.rb
+++ app/components/matey/visits_by_day_of_week_component.rb
@@ -1,4 +1,5 @@
 require "ahoy_matey"
+require "groupdate"
 
 class Matey::VisitsByDayOfWeekComponent < Matey::ApplicationComponent
   def initialize(visits:, time_window: 1.month, exclude_days: [], color_scheme: "neutral")
@@ -6,12 +7,8 @@ class Matey::VisitsByDayOfWeekComponent < Matey::ApplicationComponent
 
     visits_by_day_of_week = visits
       .where(started_at: time_window.ago..)
-      .group("strftime('%w', started_at)")
+      .group_by_day_of_week(:started_at, format: "%A")
       .count
-
-    # Map numerical days back to day names
-    days = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
-    visits_by_day_of_week = visits_by_day_of_week.transform_keys { |k| days[k.to_i] }
 
     # Remove days to be excluded
     exclude_days.each do |day|
