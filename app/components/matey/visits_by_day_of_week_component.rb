beed635ab7db43730ee5299f3c519eb59661fd35
Chris Young <chris.young@jdcp.forces.gc.ca>
2023-09-14 17:21:51 -0400

    feat: version bump

--- app/components/matey/visits_by_day_of_week_component.rb
+++ app/components/matey/visits_by_day_of_week_component.rb
@@ -3,19 +3,15 @@ require "groupdate"
 
 class Matey::VisitsByDayOfWeekComponent < Matey::ApplicationComponent
   def initialize(visits:, time_window: 1.month, exclude_days: [], color_scheme: "neutral")
+    @visits = visits
     @time_window = time_window
-
-    visits_by_day_of_week = visits
-      .where(started_at: time_window.ago..)
-      .group_by_day_of_week(:started_at, format: "%A")
-      .count
-
-    # Remove days to be excluded
-    exclude_days.each do |day|
-      visits_by_day_of_week.delete(day.capitalize)
-    end
-
-    @visits_by_day_of_week = visits_by_day_of_week
+    @exclude_days = exclude_days
     @color_scheme = color_scheme(scheme: color_scheme)
+  end
+
+  # group visits by day in provided time window
+  def visits_by_day_of_week
+    visits_by_day_of_week = @visits.group_by_day_of_week(:started_at, format: "%A", range: @time_window.ago..).count
+    visits_by_day_of_week.except!(*@exclude_days) if @exclude_days.any?
   end
 end
